40783a7b9dfe26cf272e52a822c5b6cace5897db
PeteEmbleton <peter@embleton.info>
2022-12-26 19:55:27 +1000

    Update turn_mon_on.sh

--- turn_mon_on.sh
+++ turn_mon_on.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-/usr/bin/xset -display :0.0 dpms force on
+
+#with screen blanking allowed
+#/usr/bin/xset -display :0.0 dpms force on
+
+#with screen blanking turned off
+DISPLAY=:0 xrandr --output HDMI-1 --auto
